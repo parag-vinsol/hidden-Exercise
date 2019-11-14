d581c4ad584fd6a6550b4a8d869d7d1cfbe2f705
parag-vinsol <parag@vinsol.com>
2019-11-14 19:34:49 +0530

    second commit

--- js/hiddenExercise.js
+++ js/hiddenExercise.js
@@ -1,127 +1,130 @@
-class ProductGrid {
-  constructor(colorDivId, brandDivId, productDivId, toggleButtonDivId, jsonFilePath) {
-    this.colorDiv = $(colorDivId);
-    this.brandDiv = $(brandDivId);
-    this.productDiv = $(productDivId);
-    this.availableProductButtonDiv = $(toggleButtonDivId);
-    this.jsonFilePath = jsonFilePath;
-    this.filteredProduct = [];
-    this.filters = [];
-    this.firsrFilteredResult = [];
+  class Product {
+    constructor(jsonObject) {
+    this.name = jsonObject["name"];
+    this.url = jsonObject["url"];
+    this.color = jsonObject["color"];
+    this.brand = jsonObject["brand"];
+    this.sold_out = jsonObject["sold_out"];
+    }
   }
-  init = () => {
-    $.getJSON(this.jsonFilePath).done(this.loadJSONData).fail(this.loadFailMessage);
-  }
-  loadJSONData = (data) => {
+  class ProductGrid {
+    constructor(jsonfilePath, filters) {
+    this.jsonfilePath = jsonfilePath;
+    this.filters = filters;
+    this.productDiv = $("#products");
+    this.filtersSelected = [];
+    }
+    init() {
+    $.getJSON(this.jsonfilePath).done(this.loadJSONData).fail(this.loadingFailed);
+    }
+    loadJSONData = (data) => {
     this.jsonData = data;
+    this.loadFilters();
     this.loadPage();
-  }
-  loadFailMessage = () => {
-    this.productDiv.html("Page can't be loaded");
-  }
-  loadPage() {
-    let uniqueColor = [];
-    let uniqueBrands = [];
-    for(let i = 0; i < this.jsonData.length; i++) {
-      if(uniqueColor.indexOf(this.jsonData[i]["color"]) == -1) {
-        uniqueColor.push(this.jsonData[i]["color"]);
-      }
-      if(uniqueBrands.indexOf(this.jsonData[i]["brand"]) == -1) {
-        uniqueBrands.push(this.jsonData[i]["brand"]);
-      }
-      this.filteredProduct = this.jsonData;
-      this.displayProducts(this.filteredProduct, false);
     }
-    for(let i = 0; i < uniqueColor.length; i++) {
-      let listOfColors = $("<li />",{color: uniqueColor[i],text: uniqueColor[i]}).append($("<input />", {type: "checkbox","data-checkboxKind": "filter", "data-kind": "color", "data-color": uniqueColor[i]}));
-      this.colorDiv.append(listOfColors);
+    loadingFailed = () => {
+    console.log("failed");
     }
-    for(let i = 0; i < uniqueBrands.length; i++) {
-      let listOfBrands = $("<li />",{brand: uniqueBrands[i], text: uniqueBrands[i]}).append($("<input />", {type: "checkbox","data-checkboxKind": "filter", "data-kind": "brand", "data-brand": uniqueBrands[i]}));
-      this.brandDiv.append(listOfBrands);
-    }
-    this.availableProductButtonDiv.append($("<input />",{type: "checkbox",value: "Available Products", "data-checkboxKind": "Availability", "data-button":"available Products"})).append("Available Products");
-    $("[data-checkboxKind='filter']").click(this.filterOut);
-    this.availableProductButtonDiv.find("[data-checkboxKind='Availability']").click(this.showAvailableProducts);
-  }
-  filterOut = (event) => {
-    let checkedBox = $(event.target);
-    this.filters.push(checkedBox);
-    let brandFilterSet = new Set();
-    let colorFilterSet = new Set();
-    let brandFilter = [];
-    let colorFilter =[];
-    this.filteredProduct=[];
-    this.firsrFilteredResult=[]; 
-    for(let i = 0; i < this.filters.length; i++) {
-      if(this.filters[i].is(":checked") && this.filters[i].data("kind") == "color" && this.filters.indexOf(this.filters[i].data("color")) == -1) {
-        colorFilterSet.add(this.filters[i].data("color"));
-      }  
-      else if(this.filters[i].is(":checked") && this.filters[i].data("kind") == "brand" && this.filters.indexOf(this.filters[i].data("brand")) == -1) {
-        brandFilterSet.add(this.filters[i].data("brand"));
-      }
-    }
-    brandFilter = Array.from(brandFilterSet);
-    colorFilter = Array.from(colorFilterSet);
-    if(colorFilter.length) {
-      for(let i = 0; i < colorFilter.length; i++) {
-        for(let j = 0; j < this.jsonData.length;j++) {
-          if(colorFilter[i] == this.jsonData[j]["color"]) {
-            this.firsrFilteredResult.push(this.jsonData[j]);
-          }
+    loadFilters = () => {
+    this.filterDiv = $("#filters")
+    this.filterProvided = new Set();
+    this.filters.forEach(filterElement => {
+        let uniqueFilter = [];
+        let uniqueFilterForBoolean = [];
+        this.jsonData.forEach(dataElement => {
+        if(Object.keys(dataElement).indexOf(filterElement["filterName"]) > -1 && uniqueFilter.indexOf(dataElement[filterElement["filterName"]]) == -1 && filterElement["filterType"] == "multiple") {
+            uniqueFilter.push(dataElement[filterElement["filterName"]]);
+            this.filterProvided.add(filterElement["filterName"]);
         }
-      }
-    }
-    else if(!colorFilter.length) {
-      for(let i= 0; i < brandFilter.length; i++) {
-        for(let j = 0; j < this.jsonData.length;j++) {
-          if(brandFilter[i] == this.jsonData[j]["brand"]) {
-            this.firsrFilteredResult.push(this.jsonData[j]);
-          }
+        else if(Object.keys(dataElement).indexOf(filterElement["filterName"]) > -1 && uniqueFilter.indexOf(dataElement[filterElement["filterName"]]) == -1 && filterElement["filterType"] == "boolean") {
+            uniqueFilterForBoolean.push(filterElement["filterName"]);
         }
-      }
-    }
-    if(brandFilter.length) {
-      for(let i = 0; i < brandFilter.length; i++) {
-        for(let j = 0; j < this.firsrFilteredResult.length; j++) {
-          if(this.firsrFilteredResult[j]["brand"] == brandFilter[i]) {
-            this.filteredProduct.push(this.firsrFilteredResult[j]);
-          }
+        })
+        this.filterDiv.append($("<B />", {text: `${filterElement["filterName"]} Filters`}));
+        uniqueFilter.forEach(element => {
+        this.filterDiv.append($("<li />", {value: element, text: element}).append($("<input />",{type: "checkbox", "data-filterKind": filterElement["filterName"], "data-filterName": element, "data-filterType": filterElement["filterType"]})));
+        })
+        if(uniqueFilterForBoolean.length) {
+        this.filterDiv.append($("<br>")).append($("<input />", {type: "checkbox", "data-filterType": filterElement["filterType"]})).append("Available Products");
         }
-      }
+    });
     }
-    if(!this.filteredProduct.length && colorFilter.length) {
-      this.filteredProduct = this.firsrFilteredResult;
+    loadPage = () => {
+    this.filteredProduct = this.jsonData;
+    this.displayProduct(this.filteredProduct, false);
+    this.multipleFilters = $("[data-filterType='multiple']");
+    this.booleanFilters = $("[data-filterType='boolean']");
+    this.multipleFilters.click(this.filterOut);
+    this.booleanFilters.click(this.filterAvailability);
     }
-    else if(!this.filteredProduct.length && !colorFilter.length) {
-      this.filteredProduct = this.firsrFilteredResult;
+    filterOut = (event) => {
+    let checkBox = $(event.target);
+    let filterAlreadyPresent = 0;
+    let filterKind = [];
+    this.filteredProduct = this.jsonData;
+    for (let i = 0; i < this.filtersSelected.length; i++) {
+        if(this.filtersSelected[i].data("filtername") == checkBox.data("filtername")) {
+        this.filtersSelected.splice(i, 1);
+        i--;
+        filterAlreadyPresent = 1;
+        }
     }
-    if(!brandFilter.length && !colorFilter.length) {
-      this.filteredProduct = this.jsonData;
+    if(!filterAlreadyPresent) {
+        this.filtersSelected.push(checkBox);
     }
-    this.displayProducts(this.filteredProduct, this.availableProductButtonDiv.find("[data-checkboxKind='Availability']").is(":checked"));
-  }
-  showAvailableProducts = (event) => {
-    this.displayProducts(this.filteredProduct, $(event.target).is(":checked"));
-  }
-  displayProducts = (filteredProduct, availabilityCheckbox) => {
+    this.seperateFiltersBasedOnKind();
+    this.totalFiters.forEach(element => {
+        if(element.length) {
+        this.ProductsBasedOnFilters(element, this.filteredProduct);
+        }
+    });
+    this.displayProduct(this.filteredProduct, this.booleanFilters.is(":checked"));
+    }
+    seperateFiltersBasedOnKind = () => {
+    this.totalFiters = [];
+    this.filterProvided.forEach(element => {
+        let oneTypeOfFilters = []
+        this.filtersSelected.forEach(filterElement => {
+        if(element == filterElement.data("filterkind")) {
+            oneTypeOfFilters.push(filterElement.data("filtername"))
+        }
+        });
+        this.totalFiters.push(oneTypeOfFilters);
+    });
+    }
+    ProductsBasedOnFilters = (filters, products) => {
+    let tempProducts = [];
+    filters.forEach(filterElement => {
+        products.forEach(productElement => { 
+        if(Object.values(productElement).indexOf(filterElement) != -1) {
+            tempProducts.push(productElement);
+        }
+        });
+    });
+    this.filteredProduct = tempProducts;
+    }
+    filterAvailability = (event) => {
+    this.displayProduct(this.filteredProduct, $(event.target).is(":checked"));
+    }
+
+    displayProduct = (filteredProduct, availabilityCheckbox) => {
     var products = []; 
-    if(availabilityCheckbox) {
-      for(let i = 0; i < filteredProduct.length; i++) {
-        if(filteredProduct[i]["sold_out"] == "0") {
-          products.push($("<li />", {name: filteredProduct[i]["name"]}).append($("<img />", {src:"images/" + filteredProduct[i]["url"]})));
+        if(availabilityCheckbox) {
+        for(let i = 0; i < filteredProduct.length; i++) {
+            if(filteredProduct[i]["sold_out"] == "0") {
+            products.push($("<li />", {name: filteredProduct[i]["name"]}).append($("<img />", {src:"images/" + filteredProduct[i]["url"]})));
+            }
         }
-      }
+        }
+        else {
+        for(let i = 0; i < filteredProduct.length; i++) {
+            products.push($("<li />", {name: filteredProduct[i]["name"]}).append($("<img />", {src:"images/" + filteredProduct[i]["url"]})));
+        }
+        }
+        this.productDiv.html(products);
     }
-    else {
-      for(let i = 0; i < filteredProduct.length; i++) {
-        products.push($("<li />", {name: filteredProduct[i]["name"]}).append($("<img />", {src:"images/" + filteredProduct[i]["url"]})));
-      }
-    }
-    this.productDiv.html(products);
   }
-}
 $(document).ready(function() {
-  var productGrid = new ProductGrid("#color", "#brand", "#products", "#toggle", "data/product.json");
-  productGrid.init();
+var productGrid = new ProductGrid("data/product.json",[{filterName: "color", filterType:"multiple"}, {filterName: "brand", filterType:"multiple"}, {filterName: "sold_out", filterType:"boolean"}]);
+productGrid.init();
 });
